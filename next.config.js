e42192bf0f530114a40541ce346fbb34c0ecf09c
SuperPM07 <paul@scmamparo.com.ph>
2025-02-01 18:10:38 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+};
+
+module.exports = nextConfig;
